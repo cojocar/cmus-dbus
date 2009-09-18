c5e042d5870977a23e66bf9f940a1280d6fe4661
Lucian Cojocar <cojocar@gmail.com>
2009-09-19 00:21:08 +0300

    dbus poc

---
+++ dbus-server.c
@@ -0,0 +1,111 @@
+/*
+ * Cojocar Lucian cojocar-gmail.com
+ * 
+ * cmus dbus implementation
+ * Fri Sep 18 22:07:34 EEST 2009
+ * GPL
+ */
+
+#include <stdlib.h>
+#include <dbus/dbus-glib-bindings.h>
+#include <glib.h>
+#include <glib/gprintf.h>
+#include <glib-object.h>
+
+#include "config/dbus.h"
+#include "dbus-server.h"
+#include "dbus-api.h"
+#include "dbus-bindings.h"
+
+
+G_DEFINE_TYPE(DBusCmus, cmus, G_TYPE_OBJECT);
+
+static void
+cmus_init(DBusCmus *cmus)
+{
+}
+
+static void
+cmus_class_init(DBusCmusClass *cmus_class)
+{
+	dbus_g_object_type_install_info(
+			CMUS_TYPE, &dbus_glib_cmus_object_info);
+}
+
+static gpointer
+cmus_dbus_thread(gpointer data)
+{
+	GObject *obj;
+	GMainLoop *loop;
+	GError *error = NULL;
+	DBusGConnection *connection;
+	DBusGProxy *driver_proxy;
+	guint32 request_name_ret;
+
+	g_type_init();
+	loop = g_main_loop_new(NULL, FALSE);
+///*
+//*/
+	printf("aici\n");
+
+	connection = dbus_g_bus_get (DBUS_BUS_SESSION,
+	//connection = dbus_g_bus_get (DBUS_BUS_STARTER,
+			&error);
+	if (connection == NULL)	{
+		g_printerr ("Failed to open connection to bus: %s\n",
+				error->message);
+		g_error_free (error);
+		exit (1);
+	}
+	obj = g_object_new(CMUS_TYPE, NULL);
+
+	/*
+	dbus_g_object_type_install_info(
+			CMUS_TYPE, &dbus_glib_cmus_object_info);
+	*/
+	dbus_g_connection_register_g_object (connection,
+			"/media/player/cmus", obj);
+
+	driver_proxy = dbus_g_proxy_new_for_name (connection,
+			DBUS_SERVICE_DBUS,
+			DBUS_PATH_DBUS,
+			DBUS_INTERFACE_DBUS);
+
+	///*
+	if (!org_freedesktop_DBus_request_name (driver_proxy, CMUS_SERVICE_NAME,
+				0, &request_name_ret, &error)) {
+					g_printf("EEE\n");
+				}
+
+		//die ("Failed to get name", error);
+
+	if (request_name_ret != DBUS_REQUEST_NAME_REPLY_PRIMARY_OWNER) {
+		g_error ("Got result code %u from requesting name", request_name_ret);
+		exit (1);
+	}
+//	dbus_connection_setup_with_g_main(connection, NULL);
+	//*/
+	g_main_loop_run(loop);
+	g_printf("gata_dbus\n");
+	return NULL;
+}
+
+void
+cmus_dbus_start(void)
+{
+	GThread *t;
+	GError *err;
+	err = NULL;
+	/*
+	if (g_thread_supported()) {
+//		dbus_threads_init();
+		dbus_g_thread_init();
+	}*/
+	g_thread_init(NULL);
+	t = g_thread_create(cmus_dbus_thread, NULL, FALSE, &err);
+}
+
+void
+cmus_dbus_stop(void)
+{
+}
