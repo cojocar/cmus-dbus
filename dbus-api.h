24406ce396ba3d8ea4b536e51bb60a8095f52872
Lucian Cojocar <cojocar@gmail.com>
2009-10-06 18:37:43 +0300

    cmus without dbus is compiling, again

--- dbus-api.h
+++ dbus-api.h
@@ -1,10 +1,14 @@
 #ifndef  DBUS_API_INC
 #define  DBUS_API_INC
 
+#ifdef CONFIG_DBUS
+
 #include <glib.h>
 
 #include "dbus-server.h"
 
 gboolean dbus_cmus_cmd(DBusCmus *obj, char *cmd, int *ret, GError **err);
+
+#endif
 
 #endif   /* ----- #ifndef DBUS_API_INC  ----- */
--- dbus-server.h
+++ dbus-server.h
@@ -1,6 +1,21 @@
 #ifndef  DBUS_SERVER_INC
 #define  DBUS_SERVER_INC
 
+#include "config/dbus.h"
+
+enum dbus_actions {
+	DBUS_SEEK,
+	DBUS_NEXT,
+	DBUS_PREV,
+	DBUS_STOP,
+	DBUS_SORTED_ENTER,
+	DBUS_PL_ENTER,
+	DBUS_TREE_ENTER,
+	DBUS_AUTONEXT,
+};
+
+#ifdef	CONFIG_DBUS
+
 #define CMUS_TYPE	(cmus_get_type())
 #define CMUS_SERVICE_NAME	"media.player.cmus"
 
@@ -14,20 +29,19 @@ typedef struct {
 	GObjectClass parent;
 } DBusCmusClass;
 
-enum dbus_actions {
-	DBUS_SEEK,
-	DBUS_NEXT,
-	DBUS_PREV,
-	DBUS_STOP,
-	DBUS_SORTED_ENTER,
-	DBUS_PL_ENTER,
-	DBUS_TREE_ENTER,
-	DBUS_AUTONEXT,
-};
 
+GType cmus_get_type(void);
 void cmus_dbus_hook(enum dbus_actions);
 void cmus_dbus_start(void);
 void cmus_dbus_stop(void);
-GType cmus_get_type(void);
+#else	/* !CONFIG_DBUS */
+
+#define cmus_get_type()
+#define cmus_dbus_hook(a)
+#define cmus_dbus_start()
+#define cmus_dbus_stop()
+
+#endif  /* !CONFIG_DBUS */
+
 
 #endif   /* ----- #ifndef DBUS_SERVER_INC  ----- */
